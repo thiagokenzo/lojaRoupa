8975790725749771bd3b7e90ae4e5d4501671909
thiagokenzo2002 <RM87150@fiap.com.br>
2022-10-10 21:07:38 -0300

    Update products

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -2,48 +2,48 @@ const produtos = [
     {
         id: 1,
         image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQfRObmu4mxscpDAr2363RfFKE5gfukneMhjYBbJP3mgVUQi0Cuvr3jebHKnGuQdgkPPpbcExkjjGiEew_Cv5SjZKMbnFndvYl_HuR9rqg&usqp=CAE',
-        title: 'Produto 01',
-        description: 'Descrição do produto 01',
+        title: 'Vestido Hering',
+        description: 'vestido longo vermelho | tamanho: M',
         value: 100.00
     },
 
     {
         id: 2,
         image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTC2p1VQ1-iBIBk8eWIyQRwG6kEwUJo3ts2-x75K1KbsErq2snuGJII5k3dCLWspzng7UnSnbN1Sym6z2ctdlZ7-OcMj2dbaH7eO-ntfmAt&usqp=CAE',
-        title: 'Produto 02',
-        description: 'Descrição do produto 02',
+        title: 'Vestido Zara',
+        description: 'vestido longo rosa | tamanho: M',
         value: 100.00
     },
 
     {
         id: 3,
         image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRWySd0WpIT7yBkXAUl-tAzECjB_-tpZ86L2bkVTxZSg61XLDana7g7J9zWnAh5uYm_oSuPf-DU-ZRLYcPacPDBW6KtnTD3HxWCVp-IfloXmpbrFD8iH_Yh&usqp=CAE',
-        title: 'Produto 03',
-        description: 'Descrição do produto 03',
+        title: 'Calça Renner',
+        description: 'calça preta | tamanho: 36',
         value: 50.00
     },
 
     {
         id: 4,
         image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQneAKBOShG-xfw_m1qEyDBh_8TwSUhQMw-SnXMppAhYPXQ64Vtz7gRH-pSnphdpoQRlNEzabouM3X9trEzyJfH-KFWgYbY&usqp=CAE',
-        title: 'Produto 04',
-        description: 'Descrição do produto 04',
+        title: 'Jeans Levi´s',
+        description: 'calça jeans | tamanho: 36',
         value: 30.00
     },
 
     {
         id: 5,
         image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQwQDZSCNnNu4VTFTOKFJyw2nAQLywHk-K9ubrgniVQSmw4zjIKi2dMC6Fnf6aDGUcf7uZp7KR03RcdfHMpOTrTlKoGleYQBsseZgLFOmc&usqp=CAE',
-        title: 'Produto 05',
-        description: 'Descrição do produto 05',
+        title: 'Shorts Jeans John John',
+        description: 'shorts jeans preto | tamanho: 36',
         value: 30.00
     },
 
     {
         id: 6,
         image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTzOKzCCfRhneY4amnwE187hO8843P-4N-NzmKY1qK9SpM2lkO0dnt5MYEjdHSull7gEJMNCKY54z1UXAiKDfqvN0LPgKMMNrLa79G7IJM9&usqp=CAE',
-        title: 'Produto 06',
-        description: 'Descrição do produto 06',
+        title: 'Shorts Jeans Calvin Klein',
+        description: 'shorts jeans cinza | tamanho: 36',
         value: 35.00
     }
 ]
